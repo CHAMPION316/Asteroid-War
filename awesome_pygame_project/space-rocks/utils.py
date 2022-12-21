8023fa16ea208b242bbfb8b243f03be9db6d12a4
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2022-12-21 01:40:58 -0500

    Create utils.py file for off loading images from assets folder

---
+++ awesome_pygame_project/space-rocks/utils.py
@@ -0,0 +1,10 @@
+from pygame.image import load
+
+def load_sprite(name, with_alpha=True):
+    path = f"asses/sprites/{name}.png"
+    loaded_sprite = load(path)
+    
+    if with_alpha:
+        return loade_sprite.convert_alpha()
+    else:
+        return loaded_sprite.convert(w)
