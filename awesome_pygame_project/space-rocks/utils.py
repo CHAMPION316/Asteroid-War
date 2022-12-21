727e1a4ab7191cb365ede0e551689d2296ce12d6
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2022-12-21 02:24:33 -0500

    Fix code on all files to match websites to fix error

--- awesome_pygame_project/space-rocks/utils.py
+++ awesome_pygame_project/space-rocks/utils.py
@@ -1,10 +1,11 @@
 from pygame.image import load
 
+
 def load_sprite(name, with_alpha=True):
-    path = f"asses/sprites/{name}.png"
+    path = f"assets/sprites/{name}.png"
     loaded_sprite = load(path)
-    
+
     if with_alpha:
-        return loade_sprite.convert_alpha()
+        return loaded_sprite.convert_alpha()
     else:
         return loaded_sprite.convert()
